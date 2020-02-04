1f9a0e8380ce1a56ff2a6abb08dba5c681eb401c
Cosmin <cosminp9@gmail.com>
2020-02-05 01:26:33 +0200

    Updates

---
+++ precache-manifest.5bef2e99910149fd9fe089a5b43d9d31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22eaf38fba20ee0c0896da0faa0a5827",
+    "url": "/Comi9/lp-slides/index.html"
+  },
+  {
+    "revision": "b1df4e792c4809107b7f",
+    "url": "/Comi9/lp-slides/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "ef4805c60807b55ac6de",
+    "url": "/Comi9/lp-slides/static/css/main.412c4e9f.chunk.css"
+  },
+  {
+    "revision": "b1df4e792c4809107b7f",
+    "url": "/Comi9/lp-slides/static/js/2.7f7788ea.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Comi9/lp-slides/static/js/2.7f7788ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4805c60807b55ac6de",
+    "url": "/Comi9/lp-slides/static/js/main.fc65ec77.chunk.js"
+  },
+  {
+    "revision": "fb8e1d1160f2614a660e",
+    "url": "/Comi9/lp-slides/static/js/runtime-main.d138db27.js"
+  }
+]);
